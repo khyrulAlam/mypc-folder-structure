6468e76bbddcd457e20fe66f9a76278c97c68e29
khyrulAlam <khyrulalam69@gmail.com>
2019-05-09 02:03:53 +0600

    step one done

--- router.js
+++ router.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const fs = require("fs");
+const routeDir = "/home/aa";
 
 router.get("/", (req, res) => {
   res.render(__dirname + "/public/" + "index.html");
@@ -11,16 +12,30 @@ router.get("/file", async (req, res) => {
   res.json(data);
 });
 
+router.get("/sub", async (req, res) => {
+  let { dir } = req.query;
+  try {
+    let data = await walk(dir);
+    res.json(data);
+  } catch (err) {
+    res.json(err);
+  }
+});
+
 const walk = async dir => {
   let result = [];
   let fileName = await fs.readdirSync(dir);
   fileName.map(file => {
     let realFile = file.match(/^(?![.])(?!.*[-_.]$).+/g);
     if (realFile) {
-      result.push(realFile);
+      let obj = {
+        fileName: file,
+        path: dir + "/" + file,
+        isDirectory: fs.lstatSync(dir + "/" + file).isDirectory()
+      };
+      result.push(obj);
     }
   });
-  //   str2.match(/^(?![.])(?!.*[-_.]$).+/g)
   return result;
 };
 module.exports = router;
